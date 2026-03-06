500cdab4ba234fefb833b05653113be9f7995c59
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-03-06 14:52:49 +0530

    All

---
+++ Programs/Add.java
@@ -0,0 +1,17 @@
+class Addition2Num
+{
+public static void main(String args[])
+{
+int a=5;
+int b=3;
+int c;
+c=a+b;
+System.out.print("Addition of given Two Numbers:");
+System.out.println(c);
+
+}
+}
+
+
+
+
